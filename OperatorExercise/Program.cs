51c877fd6d2a6aa65e24231a393aeffffbbc5809
Hayden Faw <faw.hayden@gmail.com>
2022-02-28 14:17:12 -0600

    initial commit

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = 1 % b;
+
+            if (a==17 && b == 4)
+            {
+                Console.WriteLine($"{a}/{b} is remainder {remainder}");
+            }
+        }
+    }
+}
